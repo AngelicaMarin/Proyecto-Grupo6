33b2dd64043e10932c6be4eb47cf333e4f4ce357
Angelica Marín <marinange22@gmail.com>
2023-03-14 21:00:44 -0600

    Creación Proyecto

---
+++ src/main/java/com/GamesCR/GamesCrApplication.java
@@ -0,0 +1,13 @@
+package com.GamesCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesCrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesCrApplication.class, args);
+	}
+
+}
