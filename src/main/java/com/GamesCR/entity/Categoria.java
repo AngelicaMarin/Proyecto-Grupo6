47ed4d920993915a8089d65ed496ceb6e2d8a1f4
Angelica Marín <marinange22@gmail.com>
2023-04-03 21:40:46 -0600

    Creacion de tienda y base de datos

---
+++ src/main/java/com/GamesCR/entity/Categoria.java
@@ -0,0 +1,36 @@
+package com.GamesCR.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+
+@Entity
+@Table (name="categorias")
+public class Categoria implements Serializable{
+    @Id
+    @GeneratedValue (strategy=GenerationType.IDENTITY)
+    private long id;
+    private String categoria;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+}
---
+++ src/main/java/com/GamesCR/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.GamesCR.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table (name="productos")
+public class Producto implements Serializable{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String producto;
+    private String precio;
+    private String descripcion;
+    private String garantia;
+    private String cant_disp;
+    
+    @ManyToOne
+    @JoinColumn (name= "categorias_id")
+    private Categoria categoria;
+    
+    public long getId(){
+        return id;
+    }
+    
+    public void setId(long id){
+        this.id=id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getGarantia() {
+        return garantia;
+    }
+
+    public void setGarantia(String garantia) {
+        this.garantia = garantia;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCant_disp() {
+        return cant_disp;
+    }
+
+    public void setCant_disp(String cant_disp) {
+        this.cant_disp = cant_disp;
+    }   
+    
+}
