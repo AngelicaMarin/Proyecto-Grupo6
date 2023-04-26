01e0148fd99fae5269f63b3ef1c101d06d63158d
Marcelo Araya Briceño <98577347+TheMarchelo@users.noreply.github.com>
2023-04-26 14:11:46 -0600

    cierre de sesión // I18N

--- src/main/java/com/GamesCR/WebConfig.java
+++ src/main/java/com/GamesCR/WebConfig.java
@@ -16,77 +16,25 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
+public class WebConfig implements WebMvcConfigurer{
+    
     @Bean
-    public SessionLocaleResolver localeResolver() {
+    public SessionLocaleResolver localResolver(){
         var slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("es"));
+        slr.setDefaultLocale(new Locale("en"));
         return slr;
     }
-
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
+    public LocaleChangeInterceptor localeChangeInterceptor(){
         var lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang"); // Aquí debes usar el nombre de atributo que definiste en el SessionLocaleResolver
+        lci.setParamName("lang");
         return lci;
     }
-
     @Override
-    public void addInterceptors(InterceptorRegistry registro) {
+    public void addInterceptors(InterceptorRegistry registro){
         registro.addInterceptor(localeChangeInterceptor());
     }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource
-                = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
-
-    /*controladores de error*/
-    @Controller
-    public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-
-        @RequestMapping("/error")
-        public String handleError(HttpServletRequest request) {
-            // Obtener el código de estado de la solicitud
-            Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-
-            // Redirigir a la página de error correspondiente según el código de estado
-            if (statusCode == 404) {
-                return "error-404";
-            } else if (statusCode == 500) {
-                return "error-500";
-            } else {
-                return "error";
-            }
-        }
-
-        public String getErrorPath() {
-            return "/error";
-        }
-    }
-
-    @Controller
-    public class LanguageController {
-
-        @RequestMapping("/language")
-        public String changeLanguage(@RequestParam("lang") String language, HttpServletRequest request) {
-            request.getSession().setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME, new Locale(language));
-            return "redirect:/"; // redirecciona al inicio después de cambiar el idioma
-        }
-        
-       
-    /*public void addViewControllers(ViewControllerRegistry registro){
-        registro.addViewController("/").setViewName("index");
-        registro.addViewController("/login");
-        registro.addViewController("/errores/403").setViewName("/errores/403");*/
-    }
-    }
-
+}
 
     
 
